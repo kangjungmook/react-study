dc52d521ddf69e098149120b62e2e0b272e648ff
kangjungmook <k01088219732@gmail.com>
2023-09-08 18:07:14 +0900

    api이용
    api이용

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherAPI from './WeatherAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherAPI/>
+    </div>
+  );
+}
+
+export default App;
